c553e22b31a83b679a9c2d5a234f73d4d51f3a60
rogerio_pinheiro <rogerps@tecgraf.puc-rio.br>
2024-03-26 19:51:38 -0300

    Segundoicommit

---
+++ Exemplo com Splitter/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Exemplo_com_Splitter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Exemplo com Splitter/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exemplo_com_Splitter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CadastrarProdutoClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Executando Cadastrar Produto", "Em Execução", MessageBoxButton.OKCancel);
+        }
+
+        private void AlterarProdutoClick(object sender, RoutedEventArgs e)
+        {
+            //AlterarProdutoWindow janela = new AlterarProdutoWindow();
+            //janela.ShowDialog();
+
+            AlterarProdutoWindow janela = new AlterarProdutoWindow() { Owner = this };
+            janela.Show();
+        }
+
+        private void RemoverProdutoClick(object sender, RoutedEventArgs e)
+        {
+            RemoverProdutoControl controle = new RemoverProdutoControl();
+            PainelDireito.Children.Add(controle);
+        }
+    }
+}
